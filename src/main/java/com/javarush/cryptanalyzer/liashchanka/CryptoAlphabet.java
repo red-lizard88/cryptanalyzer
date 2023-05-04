cf6d40cfbcadc96380da2bc91a3fb3be0f6b1e58
Liashchanka Ilya <i.liashchanka@gmail.com>
2023-05-04 17:07:51 +0300

    Made Coding, Encoding and BrutForce

--- src/main/java/com/javarush/cryptanalyzer/liashchanka/CryptoAlphabet.java
+++ src/main/java/com/javarush/cryptanalyzer/liashchanka/CryptoAlphabet.java
@@ -4,7 +4,7 @@ public class CryptoAlphabet {
     private static final String lettersUpperCase = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     private static final String lettersLowerCase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     private static final String numbers = "0123456789";
-    private static final String symbols = ".,\"\":-!? ";
+    private static final String symbols = ".,\":-!? ";
 
     public static final String ALPHABET = lettersUpperCase + lettersLowerCase + numbers + symbols;
 
